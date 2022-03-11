44aefee58184ebf87d49a18e1060d93f35930d13
Sergei Gridasov <44599468+jzzsml@users.noreply.github.com>
2022-03-11 19:41:13 +0300

    update logic

--- lib/ruby_process_info.rb
+++ lib/ruby_process_info.rb
@@ -2,12 +2,18 @@
 
 require 'rusage'
 
-module RubyProcessInfo
+class RubyProcessInfo 
+  def self.rss_in_mb
+    rss_in_kb / 1024
+  end
+  
+  private
+  
   def rss
     ::Process.rusage.maxrss
   end
 
-  def rss_in_mb
+  def rss_in_kb
     rss.to_f / 1024
   end
 end
