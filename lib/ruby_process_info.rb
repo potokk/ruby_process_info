397d08644fa05486fec0056e852c0e884791b6b7
Sergei Gridasov <44599468+jzzsml@users.noreply.github.com>
2022-03-11 19:49:11 +0300

    Update ruby_process_info.rb

--- lib/ruby_process_info.rb
+++ lib/ruby_process_info.rb
@@ -7,13 +7,11 @@ class RubyProcessInfo
     rss_in_kb / 1024
   end
   
-  private
-  
-  def rss
+  def self.rss
     ::Process.rusage.maxrss
   end
 
-  def rss_in_kb
+  def self.rss_in_kb
     rss.to_f / 1024
   end
 end
