93391a0f0208b2303287da2bbd48eb2acee9fc6b
Sergei Gridasov <44599468+jzzsml@users.noreply.github.com>
2022-03-15 18:28:30 +0300

    Update ruby_process_info.rb

--- lib/ruby_process_info.rb
+++ lib/ruby_process_info.rb
@@ -4,14 +4,12 @@ require 'rusage'
 
 class RubyProcessInfo 
   def self.rss_in_mb
-    rss_in_kb / 1024
+    kilobyte_adjust = ((/darwin/ =~ RUBY_PLATFORM) != nil) ? 1024 : 1
+    
+    rss.to_f / 1024 / kilobyte_adjust
   end
   
   def self.rss
     ::Process.rusage.maxrss
-  end
-
-  def self.rss_in_kb
-    rss.to_f / 1024
   end
 end
